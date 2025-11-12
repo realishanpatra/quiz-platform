7fbbba79d47005d20fc4a245d56d66e4e2080ed8
Ishan Patra <ishan.patran102@gmail.com>
2025-11-13 00:39:18 +0530

    New2

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/page.tsx
-export default function HomePage() {
-  return (
-    <div style={{ maxWidth: 900, margin: "2rem auto" }}>
-      <h1>Welcome to Quiz Platform</h1>
-      <p>Create quizzes as a teacher. Students can take quizzes and view results.</p>
-    </div>
-  );
-}
